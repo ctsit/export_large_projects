ad089cf6594962c5ea08e7d85fc683084200cb8d
suryayalla <prasannakumar006@gmail.com>
2018-03-15 10:53:33 -0400

    show export large projects button only in All Data field

--- js/export_project_button.js
+++ js/export_project_button.js
@@ -3,6 +3,6 @@ $(document).ready(function() {
     var str = exportLargeProjects.button_html;
     var $newButton = $(str);
 
-    $('.rprt_btns').append($newButton);
+    $(‘#reprow_ALL .rprt_btns').append($newButton);
 
 });
